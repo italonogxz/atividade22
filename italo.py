0d4406eebe5ede41d5fedb5aabb3f243a1370074
italonogueira <147431079+italonogxz@users.noreply.github.com>
2023-10-30 13:57:40 -0300

    Add files via upload

---
+++ italo.py
@@ -0,0 +1,7 @@
+# Exercício Python 22: Faça um programa que mostre na tela os numeros pares entre 1 e 50 (dica estude range e seus paramentros)
+
+print("numeros pares!!")
+for n in range(2, 51, 2):
+    print(n)
+
+ 
